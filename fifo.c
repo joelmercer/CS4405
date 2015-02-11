150a062775c8ae10782b98b947329a27533fe1bb
joelmercer <joelmercer@Joels-MacBook-Pro.local>
2015-02-11 17:34:26 -0400

    fifo.c and init.c

---
+++ fifo.c
@@ -0,0 +1,5 @@
+#include<os.h>
+
+//FIFO  OS_InitFiFo();\
+//void  OS_Write( FIFO f, int val );\
+//BOOL  OS_Read( FIFO f, int *val );\
---
+++ init.c
@@ -0,0 +1,6 @@
+#include<os.h>
+
+/* OS Initialization */\
+//void OS_Init();\
+//void OS_Start();\
+//void OS_Abort();\
