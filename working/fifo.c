cb389696e4977fa7d602b2022712a0f47e03630d
f4hfl <f4hfl@unb.ca>
2015-02-16 10:20:48 -0400

    FIFO Fix

--- working/fifo.c
+++ working/fifo.c
@@ -12,14 +12,9 @@ FIFO OS_InitFiFo() {
 		node n;
 		n.flag = 0; // This allows the value to be overwritten, 0==read
 		fifoarray[fifocounter]* = n; // First node, also the "head" node
-<<<<<<< HEAD
-<<<<<<< HEAD
 		node* first = n;
-=======
->>>>>>> origin/master
-=======
->>>>>>> 00cd4ce7b2f56c7c13d4f006d49a75c65764e47d
-		for(i =1; i<=FIFOSIZE){
+		
+		for(i =1; i<=FIFOSIZE; i++){
 			node m;
 			m.flag = 0;
 			fifoarray[fifocounter].next* = m;
