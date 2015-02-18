7b931d97b6702f69275e36a0f7b3b4bbee16ee58
f4hfl <f4hfl@unb.ca>
2015-02-17 23:47:32 -0400

    FiFo Pointers
    I have the pointers all sorted out with the exception of 3 errors that I
    cannot seem to make go away

--- working/fifo.c
+++ working/fifo.c
@@ -9,18 +9,18 @@ FIFO OS_InitFiFo() {
 	} else {
 		node n;
 		n.flag = 0; // This allows the value to be overwritten, 0==read
-		fifoarray[fifocounter]* = n; // First node, also the "head" node
-		node* first = n;
+		fifoarray[fifocounter] = n; // First node, also the "head" node
+		node first = n;
 		
 		for(i =1; i<=FIFOSIZE; i++){
 			node m;
 			m.flag = 0;
-			fifoarray[fifocounter].next* = m;
-			m.previous* = fifoarray[fifocounter];
-			fifoarray[fifocounter]* = m;
+			fifoarray[fifocounter].next = &m;
+			m.previous = &fifoarray[fifocounter];
+			fifoarray[fifocounter] = m;
 		}
-		fifoarray[fifocounter].next = first;
-		first.previous* = fifoarray[fifocounter];
+		fifoarray[fifocounter].next = &first;
+		first.previous = &fifoarray[fifocounter];
 	}
 	fifocounter++;
 	retval = fifocounter; // place in fifo array where fifo begins?
@@ -29,16 +29,16 @@ FIFO OS_InitFiFo() {
 
 void OS_Write(FIFO f, int val) {
 	int i=0;
-	node *pointer=*fifoarray[f]; // find proper fifo
+	node pointer=fifoarray[f]; // find proper fifo
 	while(i<FIFOSIZE){ //check for free space
 		if((pointer.flag==1)&&(i==FIFOSIZE)){ //FIFO full
-			*fifoarray[f].data=&val;
-			*fifoarray[f].flag=0;
-			*fifoarray[f]=&fifoarray[f].next;
+			fifoarray[f].data=val;
+			fifoarray[f].flag=0;
+			fifoarray[f]=&fifoarray[f].next;
 			return;
 		}
 		if(pointer.flag==1){
-			pointer=pointer.next;
+			pointer=&pointer.next;
 			i++;
 		}else{ //write
 			pointer.data=val;
@@ -50,13 +50,13 @@ void OS_Write(FIFO f, int val) {
 
 BOOL OS_Read(FIFO f, int *val) {
 	int i=0;
-	node *pointer=&fifoarray[f]; // find proper fifo
+	node pointer=fifoarray[f]; // find proper fifo
 	if(pointer.flag==1){ // fifo is empty
 		return FALSE;
 	} else{
-		val=*pointer.data;
+		val=&pointer.data;
 		pointer.flag=1;// mark as read
-		*fifoarray[f]=&pointer.next;
+		fifoarray[f]=&pointer.next;
 		return TRUE;
 	}
 	
